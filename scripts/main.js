e8c0da3d45977df4cdf1469584a6e5259121d887
Trnewton <thomas.newton@ucalgary.ca>
2020-04-08 15:00:01 -0600

    Added firebase init

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,7 @@ const inputEmail_span = document.getElementById("input-Email");
 const inputPassword_span = document.getElementById("input-Password");
 const loginButton_span = document.getElementById("login-button");
 
+
 main();
  
 function main() {
@@ -9,11 +10,13 @@ function main() {
     loginButton_span.addEventListener('click', verify_User);
 }
 
+
+
 function verify_User(username, password) {
     // For now we assume password/user pair is correct and print out for debuggin
     console.log("Username: " + inputEmail_span.value);
     console.log("Password: " + inputPassword_span.value);
 
-    
+
 }
 
