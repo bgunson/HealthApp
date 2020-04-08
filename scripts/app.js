53cb9e07d62a5400cfe146140bf844eca9a00c45
Trnewton <thomas.newton@ucalgary.ca>
2020-04-08 17:17:41 -0600

    Database stuff

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,21 @@
+'use strict'
+
 const inputEmail_span = document.getElementById("input-Email");
 const inputPassword_span = document.getElementById("input-Password");
 const loginButton_span = document.getElementById("login-button");
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB2j1bGOOC9pWCy7YlNcyVmmI29PMQyBR4",
+    authDomain: "health-app-502a4.firebaseapp.com",
+    databaseURL: "https://health-app-502a4.firebaseio.com",
+    projectId: "health-app-502a4",
+    storageBucket: "health-app-502a4.appspot.com",
+    messagingSenderId: "407596506008",
+    appId: "1:407596506008:web:700a3f928a6c5c772d69ed"
+  };
+  firebase.ini
+const database = firebase.database();
 
 
 main();
@@ -8,15 +23,22 @@ main();
 function main() {
     // This seems like a stupid main
     loginButton_span.addEventListener('click', verify_User);
+
 }
 
 
 
-function verify_User(username, password) {
+function verify_User() {
     // For now we assume password/user pair is correct and print out for debuggin
-    console.log("Username: " + inputEmail_span.value);
-    console.log("Password: " + inputPassword_span.value);
+    var username = inputEmail_span.value
+    var password = inputPassword_span.value
+    console.log("Username: " + username)
+    console.log("Password: " + password)
 
-
+    if (username =='Bob' && password == "Bob is cool") {
+        window.location = "pages/account.html"
+    } else {
+        alert('Bad username password!')
+    }
 }
 
